5bb139cfa5d946f20829286b62c136038f0dd1e2
Anwesh43 <anweshthecool0@gmail.com>
2017-01-06 17:15:45 +0530

    Adding CircleQuotes Component

---
+++ CircleQuotes.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+class CircleQuoteComponent extends Component {
+    constructor(props) {
+      super(props)
+    }
+    render() {
+      var divStyle = {background:'green',width:200,height:200,borderRadius:'50%',floating:'top',textAlign:'center'}
+      var textStyle = {color:'white',fontSize:20,textAlign:'center',padding:75}
+      return (<div style={divStyle}>
+          <p style={textStyle}>
+              {this.props.text}
+          </p>
+      </div>)
+    }
+}
