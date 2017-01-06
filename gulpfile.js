365eb466764a860424dfe870f0ab03699a752317
Anwesh43 <anweshthecool0@gmail.com>
2017-01-06 17:16:45 +0530

    adding gulpfile changes to compile the es6 of new file

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ gulp.task('compileES6ForExample',()=>{
 gulp.task('compileES6ForChat',()=>{
   return browserify('ChatLikeDiv.js').transform('babelify',{presets:['es2015','react']}).bundle().pipe(require('fs').createWriteStream('chat/bundle.js'))
 })
+gulp.task('compileES6ForQuoteCircle',()=>{
+  return browserify('CircleQuotes.js').transform('babelify',{presets:['es2015','react']}).bundle().pipe(require('fs').createWriteStream('quotesincircle/bundle.js'))
+})
+
 gulp.task('watchForChange',()=>{
     gulp.watch('*.js',['compileES6ForExample','compileES6ForPublic'])
 })
