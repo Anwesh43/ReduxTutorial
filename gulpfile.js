1411612843c5c3dbcf0aeb3dcd551849bbe670e5
Anwesh43 <anweshthecool0@gmail.com>
2017-01-30 19:45:43 +0530

    Adding task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,9 @@ gulp.task('compileES6ForBlockComponent',()=>{
 gulp.task('compileForDrawingApp',()=>{
     return browserify('DrawingComponent.js').transform('babelify',{presets:['es2015','react']}).bundle().pipe(require('fs').createWriteStream('drawingApp/bundle.js'))
 })
+gulp.task('compileForAnimatedCirc',()=>{
+    browserify('CircleContainerComponents.js').transform('babelify',{presets:['es2015','react']}).bundle().pipe(require('fs').createWriteStream('circs/bundle.js'))
+})
 gulp.task('watchForChange',()=>{
     gulp.watch('*.js',['compileES6ForExample','compileES6ForPublic'])
 })
