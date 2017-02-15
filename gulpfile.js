5a6a6d01446ea4e9808cdece15103c318e87a4bc
Anwesh43 <anweshthecool0@gmail.com>
2017-02-15 23:15:56 +0530

    Adding task to compile store_impl_test_component

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,9 @@ gulp.task('compileForDrawingApp',()=>{
 gulp.task('compileForAnimatedCirc',()=>{
     browserify('CircleContainerComponents.js').transform('babelify',{presets:['es2015','react']}).bundle().pipe(require('fs').createWriteStream('circs/bundle.js'))
 })
+gulp.task('compileForStoreImpl',()=>{
+    browserify('store_impl_test.js').transform('babelify',{presets:['es2015','react']}).bundle().pipe(require('fs').createWriteStream('test/bundle.js'))
+})
 gulp.task('watchForChange',()=>{
     gulp.watch('*.js',['compileES6ForExample','compileES6ForPublic'])
 })
