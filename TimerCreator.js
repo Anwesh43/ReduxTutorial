8bc6ebffb8d6881f04bb9e2b01fa300c56e00e8e
Anwesh43 <anweshthecool0@gmail.com>
2017-08-22 11:35:40 +0530

    adding TimerComponent

--- TimerCreator.js
+++ TimerCreator.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react'
 import {createStore} from 'redux'
+const w = window.innerWidth,h = window.innerHeight
 const timerCreateReducer = (state={timers:[]},action) => {
     const newState = Object.assign({},state)
       if(action.type == "CREATE") {
@@ -14,7 +15,7 @@ const timerCreateReducer = (state={timers:[]},action) => {
 }
 const timerStore = createStore(timerCreateReducer)
 
-class TimerGroupComponent {
+class TimerGroupComponent extends Component{
     constructor(props) {
         super(props)
         this.state = timerStore.getState()
@@ -25,7 +26,17 @@ class TimerGroupComponent {
         })
     }
     render() {
+        const timerComponents = this.state.timers.map((timer)=>(<TimerComponent time={timer.time}/>))
         <div>
+            {timerComponents}
         </div>
+    }
+}
+class TimerComponent extends Component{
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (<div style={{width:w/12,height:w/12,borderWidth:2,borderStyle:'solid',borderColor:'green',fontSize:30}}>{this.props.time}</div>)
     }
 }
